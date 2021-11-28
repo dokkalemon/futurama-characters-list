0b341d8ccfebf7a9ecde5f788e4e855e38bc25f5
dokkalemon <dottori.roberto@gmail..com>
2021-11-28 17:40:34 +0100

    add Header Components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,16 @@
 <template>
   <header>
-      <h1>QUI HEADER</h1>
+      <div class="header container ">
+          <img src="../assets/logo-futurama.png" alt="">
+          <h2>Character List</h2>
+          <div class="input-container">
+              <form action="">
+                  <input type="text" placeholder="Inserisci il nome di un personaggio">
+                  <button type="submit">Search</button>
+                  <button type="reset" class="reset">Reset</button>
+              </form>
+          </div>
+      </div>
   </header>
 </template>
 
@@ -10,6 +20,60 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '@/styles/vars';
 
+    header {
+        .header {
+            padding: 15px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            img {
+                height: 150px;
+            }
+            h2 {
+                color: $primary-color;
+                font-weight: 300;
+                padding-bottom: 10px;
+            }
+            .input-container {
+                width: 50%;
+                form {
+                    display: flex;
+                    input {
+                    height: 30px;
+                    background-color: transparent;
+                    border: none;
+                    border-bottom: 2px solid $primary-color;
+                    width: 100%;
+                    margin-right: 5px;
+                    color: white;
+                    font-size: 18px;
+                    outline: none;
+                    }
+                    button {
+                        background-color: $tertiary-color;
+                        border: none;
+                        border-radius: 5px;
+                        padding: 5px;
+                        margin-right: 5px;
+                        color: $primary-color;
+                        font-weight: 700;
+                        cursor: pointer;
+                        &:hover {
+                            background-color: white;
+                            color: $tertiary-color;
+                        }
+                    }
+                    .reset {
+                        background-color: $primary-color;
+                        color: $tertiary-color
+                    }
+                }
+
+            }
+        }
+    }
 </style>
